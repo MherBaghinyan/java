239f7d3106fbd846b6a265dd3743fb10eefe4b1d
mherbaghinyan <mbaghinyan@gmail.com>
2017-10-23 23:12:07 +0400

    fixed sql

--- java/src/main/src/main/resources/schema.sql
+++ java/src/main/src/main/resources/schema.sql
@@ -9,10 +9,9 @@ CREATE TABLE `request_data` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-SELECT r.ip FROM request_data r
-GROUP BY r.ip, r.start_date
-HAVING r.start_date BETWEEN ('2017-01-01.13:00:00' AND '2017-01-01.14:00:00')
-       AND count(r.ip) > 100;
+SELECT * FROM request_data
+GROUP BY ip
+HAVING count(ip) > 100 AND start_date BETWEEN '2017-01-01.13:00:00' AND '2017-01-01.14:00:00';
 
 SELECT * FROM request_data
 WHERE ip = '192.168.77.101';
